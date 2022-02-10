1b3766ec069a8101e9e3bba718ee726cbf67a532
ciscoducasse99 <ciscoducasse99@hotmail.com>
2022-02-10 11:53:05 -0500

    resolving landing error

--- server/server.js
+++ server/server.js
@@ -6,8 +6,12 @@ require("dotenv").config({ path: `${__dirname}/config/config.env` });
 const app = express();
 const PORT = process.env.PORT || 5000;
 
-app.use(express.static(path.join(process.cwd(), "client/build")));
-
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static("build"));
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(process.cwd(), "client/build", "index.html"));
+  });
+}
 app.use(express.urlencoded({ extended: false }), express.json());
 app.use("/orders", require("./routes/index"));
 
