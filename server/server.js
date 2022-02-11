d8afc3dbad8c54dff818baf4a52e32e2eb1a0677
ciscoducasse99 <ciscoducasse99@hotmail.com>
2022-02-10 22:54:33 -0500

    heroku-postbuild

--- server/server.js
+++ server/server.js
@@ -6,14 +6,15 @@ require("dotenv").config({ path: `${__dirname}/config/config.env` });
 const app = express();
 const PORT = process.env.PORT || 5000;
 
-app.use(express.static(path.join(process.cwd(), "frontend/build")));
+app.use(express.static(path.join(process.cwd(), "client/build")));
 app.use(express.urlencoded({ extended: false }), express.json());
 app.use("/orders", require("./routes/index"));
 
 app.listen(PORT, () => {
-  console.log("> Server started on port 5000");
+  console.log(`> Server started on port ${PORT}`);
 });
 
+console.log(path.join(process.cwd(), "client/build"));
 // Route4Me
 // Mysql
 // Knex
