4e7829e464a7f074da89e05aaae81ca51bf56865
ciscoducasse99 <ciscoducasse99@hotmail.com>
2022-02-10 22:40:07 -0500

    rollback

--- server/server.js
+++ server/server.js
@@ -6,12 +6,7 @@ require("dotenv").config({ path: `${__dirname}/config/config.env` });
 const app = express();
 const PORT = process.env.PORT || 5000;
 
-if (process.env.NODE_ENV === "production") {
-  app.use(express.static("build"));
-  app.get("*", (req, res) => {
-    res.sendFile(path.resolve(process.cwd(), "client/build", "index.html"));
-  });
-}
+app.use(express.static(path.join(process.cwd(), "frontend/build")));
 app.use(express.urlencoded({ extended: false }), express.json());
 app.use("/orders", require("./routes/index"));
 
